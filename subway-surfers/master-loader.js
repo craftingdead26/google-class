24ca7d3d305535d233708c40d0e95c7403dac3c6
genizy <185154684+genizy@users.noreply.github.com>
2024-12-31 14:40:40 -0800

    afafafafafafafafafafa

--- subway-surfers/master-loader.js
+++ subway-surfers/master-loader.js
@@ -14,10 +14,10 @@ if (!window.config.unityWebglLoaderUrl) {
     var versionSplit = window.config.unityVersion ? window.config.unityVersion.split(".") : [],
         year = versionSplit[0],
         minor = versionSplit[1];
-    window.config.unityWebglLoaderUrl = "https://cdn.jsdelivr.net/gh/genizy/google-class@a71ceefd192febf0244977f06093a353577feb8b/subway-surfers/UnityLoader.2019.2.js";
+    window.config.unityWebglLoaderUrl = "https://cdn.jsdelivr.net/gh/genizy/google-class@1fcbc19db198e378b4fd268bc5970b8f48b2adf8/subway-surfers/UnityLoader.2019.2.js";
 }
 var sdkScript = document.createElement("script");
-sdkScript.src = "https://cdn.jsdelivr.net/gh/genizy/google-class@a71ceefd192febf0244977f06093a353577feb8b/subway-surfers/poki-sdk.js", sdkScript.onload = function() {
+sdkScript.src = "https://cdn.jsdelivr.net/gh/genizy/google-class@1fcbc19db198e378b4fd268bc5970b8f48b2adf8/subway-surfers/poki-sdk.js", sdkScript.onload = function() {
     var i = document.createElement("script");
     i.src = root + loader, document.body.appendChild(i)
 }, document.body.appendChild(sdkScript);
